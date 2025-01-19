c27df76a3996fd123afebddacfd56209fe63c665
Maxim Shlemin <clatto@yandex.ru>
2025-01-19 19:11:32 +0300

    Added more changes for refactor

--- src/test/java/helpers/MobileAttach.java
+++ src/test/java/helpers/MobileAttach.java
@@ -3,7 +3,6 @@ package helpers;
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-
 import java.nio.charset.StandardCharsets;
 
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
