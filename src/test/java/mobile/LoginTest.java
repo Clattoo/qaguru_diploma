8f4752c17e5a5383d235d69fd99ddf8e07b9d683
Maxim Shlemin <clatto@yandex.ru>
2025-01-19 18:58:46 +0300

    Added change for test in mobile test

--- src/test/java/mobile/LoginTest.java
+++ src/test/java/mobile/LoginTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-@Tag("android")
+//@Tag("android")
 public class LoginTest extends MobileTestBase {
 
     OnboardingComponent onboardingComponent = new OnboardingComponent();
